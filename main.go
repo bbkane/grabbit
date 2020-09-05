c63eea641fda5707e5204d0fd2f5cb9d06769a9b
Benjamin Kane <bbk1524@gmail.com>
2020-09-05 14:53:00 -0700

    get some images downloading

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/vartanbeno/go-reddit/reddit"
 )
 
-var ctx = context.Background()
 
 func main() {
 	if err := run(); err != nil {
@@ -16,40 +20,87 @@ func main() {
 	}
 }
 
-func run() (err error) {
+func downloadFile(URL, fileName string) error {
+	// https://golangbyexample.com/download-image-file-url-golang/
+    //Get the response bytes from the url
+    response, err := http.Get(URL)
+    if err != nil {
+    }
+    defer response.Body.Close()
+
+    //Create a empty file
+    file, err := os.Create(fileName)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    //Write the bytes to the fiel
+    _, err = io.Copy(file, response.Body)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func fileNameFromUrl(fullUrl string) (string,  error) {
+	// https://www.golangprograms.com/golang-download-image-from-given-url.html
+    fileUrl, err := url.Parse(fullUrl)
+	if err != nil {
+		return "", err
+	}
+
+    path := fileUrl.Path
+    segments := strings.Split(path, "/")
+
+	fileName := segments[len(segments)-1]
+	return fileName, nil
+}
+
+func getTopPosts(client *reddit.Client, ctx context.Context, subredditName string, postLimit int, timeFrame string) ([]*reddit.Post, error){
+	posts, _, err := client.Subreddit.TopPosts(ctx, "earthporn", &reddit.ListPostOptions{
+		ListOptions: reddit.ListOptions{
+			Limit: 5,
+		},
+		Time: "all",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return posts, nil
+}
+
+func run() error {
 	// Let's get the top 200 posts of r/golang.
 	// Reddit returns a maximum of 100 posts at a time,
 	// so we'll need to separate this into 2 requests.
-	posts, resp, err := reddit.DefaultClient.Subreddit.TopPosts(ctx, "golang", &reddit.ListPostOptions{
-		ListOptions: reddit.ListOptions{
-			Limit: 100,
-		},
-		Time: "all",
-	})
+
+	client, err := reddit.NewReadonlyClient()
 	if err != nil {
-		return
+		return err
+	}
+
+	ctx := context.Background()
+
+	posts, err := getTopPosts(client, ctx, "earthporn", 5, "day")
+	if err != nil {
+		return err
 	}
 
 	for _, post := range posts {
-		fmt.Println(post.Title)
-	}
+		fmt.Println(post.URL)
+		if strings.HasSuffix(post.URL, ".jpg") {
 
-	// The After option sets the id of an item that Reddit
-	// will use as an anchor point for the returned listing.
-	posts, _, err = reddit.DefaultClient.Subreddit.TopPosts(ctx, "golang", &reddit.ListPostOptions{
-		ListOptions: reddit.ListOptions{
-			Limit: 100,
-			After: resp.After,
-		},
-		Time: "all",
-	})
-	if err != nil {
-		return
-	}
+			fileName, err := fileNameFromUrl(post.URL)
+			if err != nil {
+				log.Printf("fileNameFromUrl: %v: %v", post.URL, err)
+			}
 
-	for _, post := range posts {
-		fmt.Println(post.Title)
+			err = downloadFile(post.URL, fileName)
+			if err != nil {
+				log.Printf("downloadFile: %v: %v", post.URL, err)
+			}
+		}
 	}
-
-	return
+	return nil
 }
