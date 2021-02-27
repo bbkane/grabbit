8bd07f82fa23429fc48d1efde15eab4c477e5a3a
Benjamin Kane <bbk1524@gmail.com>
2021-02-27 10:42:53 -0800

    use package main in doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// Package grabbit: Grab subreddit images!! Very useful for getting nice wallpapers automatically. Designed to be easy to install/uninstall. See README.md: https://github.com/bbkane/grabbit
-package grabbit
+// Package main: Grab subreddit images!! Very useful for getting nice wallpapers automatically. Designed to be easy to install/uninstall. See README.md: https://github.com/bbkane/grabbit
+package main
