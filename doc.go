8bb6323a8e21bbfec0c5be2ace05f6a24489efd8
Benjamin Kane <bbk1524@gmail.com>
2021-02-27 21:15:14 -0800

    Rm doc.go
    Turns out pkg.go.dev indexes the README.md

--- doc.go
+++
@@ -1,2 +0,0 @@
-// Package main: Grab subreddit images!! Very useful for getting nice wallpapers automatically. Designed to be easy to install/uninstall. See README.md: https://github.com/bbkane/grabbit
-package main
